10172521d58bae163f62d92aab3fd2db99c6d989
shakerrrr <contact@bechinka.eu>
2019-09-20 00:40:09 +0200

    added main menu

--- Console-Chess/Program.cs
+++ Console-Chess/Program.cs
@@ -6,10 +6,37 @@ namespace Console_Chess
     {
         static void Main(string[] args)
         {
+            mainMenu();
+            Console.ReadKey();
+        }
+
+        static void runGame()
+        {
             Board game = new Board();
             game.CreatePieces();
             game.UpdateDraw();
-            Console.ReadKey();
+        }
+
+        static void mainMenu()
+        {
+            Console.Out.WriteLine("Console Chess MAIN MENU\n");
+            Console.Out.WriteLine("Please select a menu option:");
+            Console.Out.WriteLine("1. Start new game. | 2. Exit.");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    runGame();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.Out.WriteLine("Good bye.");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.Clear();
+                    mainMenu();
+                    break;
+            }
         }
     }
 }
